5324f1de4dbe93265f58126869417e523a2b123f
SeliyandaMafeking <148527992+SeliyandaMafeking@users.noreply.github.com>
2023-12-07 14:38:24 +0200

    Added Beer Song code to Assignment 1

---
+++ Assignement1Sln/Assignement1/BeerSong.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement1
+{
+    internal class BeerSong
+    {
+        public static string Sing(int numberOfBottles)
+        {
+            for (int i = 99; i > 1; i--)
+            { 
+                Console.WriteLine($"{WriteNumberOfBeer(i)} of beer on the wall, {WriteNumberOfBeer(i)} of beer");
+                Console.WriteLine($"Take one down and pass it around, {WriteNumberOfBeer(i - 1)} of beer on the wall");
+                Console.WriteLine("1 bottle of beer on the wall, 1 bottle of beer");
+            }
+
+
+           static string WriteNumberOfBeer(int numberOfBottles)
+            { 
+                if (numberOfBottles == 1)
+                {
+                    return "1 bottle";
+                }
+                else
+                {
+                    return $"{numberOfBottles} bottles";
+                }
+            }
+        
+            return "";
+        }
+    }
+}
---
+++ Assignement1Sln/Assignement1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+namespace Assignement1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BeerSong.Sing(99);
+
+            /*for (int i = 99; i > 0; i--)
+            {
+                Console.WriteLine($"{bottles(i)} of beer on the wall, {WriteNumberOfBottles(i)} of beer");
+                Console.WriteLine($"Take one down and pass it around, {WriteNumberOfBottles(i-1)} of beer on the wall");
+            }
+            
+            
+
+               static string WriteNumberOfBottles(int numberOfBottles )
+            {
+                if (numberOfBottles == 1)
+                {
+                    return "1 bottle";
+                }
+                else
+                { 
+                    return $"{numberOfBottles} bottles";
+                }
+               
+            }*/
+        }
+    }
+}
